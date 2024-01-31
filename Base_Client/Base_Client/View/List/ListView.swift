2b1ce65f4d12c3f0264cedb8555db3bac5dc37c5
Farhandika <18220015@std.stei.itb.ac.id>
2024-01-31 20:25:46 +0700

    Added swiftjson dependency

---
+++ Base_Client/Base_Client/View/List/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  Base_Client
+//
+//  Created by terminal on 31/01/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
